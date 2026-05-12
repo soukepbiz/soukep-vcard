4892d30602726f8866a4ca40ad1df4470418dfb0
soukepbiz <soukep.business@gmail.com>
2026-05-12 04:57:01 +0200

    Remove h-full from html element to allow page scroll on desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" className="h-full antialiased">
+    <html lang="fr" className="antialiased">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
